aa6f819d11263f6ad8ee94aadf7c9819436193fc
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-26 12:07:20 +0400

    playground with bottom navigation bar (#4)

---
+++ flutter/lib/built_in_case.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+class BuiltInCase extends StatelessWidget {
+  const BuiltInCase({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            const Text('😎'),
+            const SizedBox(width: 20),
+            Expanded(child: CupertinoTextField()),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Row(
+          children: [
+            const Text('🕒'),
+            const SizedBox(width: 20),
+            SizedBox(
+              width: 280,
+              height: 50,
+              child: CupertinoDatePicker(
+                initialDateTime: DateTime.now().add(Duration(days: -1)),
+                onDateTimeChanged: (value) {
+                  print(value);
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,80 +1,74 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:horology/cupertino.dart';
+import 'package:horology/built_in_case.dart';
+import 'package:horology/main_case.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HorologyPlaygroundApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HorologyPlaygroundApp extends StatelessWidget {
+  const HorologyPlaygroundApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Horology Demo',
+      title: 'Horology Playground',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Horology'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  const Text('😎'),
-                  const SizedBox(width: 20),
-                  Expanded(child: CupertinoTextField()),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text('🕒'),
-                  const SizedBox(width: 20),
-                  SizedBox(
-                    height: 50,
-                    child: CupertinoDateTimePicker(
-                      initialDateTime: DateTime.now().add(Duration(days: -1)),
-                      onSelectionChanged: (value) {
-                        print(value);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const HorologyPlaygroundPage(),
     );
   }
 }
 
-class TimePickerOriginal extends StatelessWidget {
-  const TimePickerOriginal({
+class HorologyPlaygroundPage extends StatefulWidget {
+  const HorologyPlaygroundPage({
     super.key,
   });
 
   @override
+  State<HorologyPlaygroundPage> createState() => _HorologyPlaygroundPageState();
+}
+
+class _HorologyPlaygroundPageState extends State<HorologyPlaygroundPage> {
+  ValueNotifier navigationBarIndex = ValueNotifier(0);
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 250,
-      height: 50,
-      child: CupertinoDatePicker(
-        mode: CupertinoDatePickerMode.dateAndTime,
-        onDateTimeChanged: (value) {
-          print(value);
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Horology'),
+      ),
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: navigationBarIndex,
+        builder: (context, pageIndex, _) => NavigationBar(
+          selectedIndex: pageIndex,
+          onDestinationSelected: (index) => navigationBarIndex.value = index,
+          destinations: [
+            NavigationDestination(
+              icon: const Text('😎'),
+              label: 'Main',
+            ),
+            NavigationDestination(
+              icon: const Text('🥱'),
+              label: 'Built-in',
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(40),
+        child: ValueListenableBuilder(
+          valueListenable: navigationBarIndex,
+          builder: (context, pageIndex, _) {
+            if (pageIndex == 0) return MainCase();
+            if (pageIndex == 1) return BuiltInCase();
+            
+            throw Exception('Invalid page index');
+          }
+        ),
       ),
     );
   }
